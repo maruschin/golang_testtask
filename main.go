fbe10ecfe8d31080d956cc57f25dff6ea391eb06
Eugene Maruschin <maruschin@gmail.com>
2017-11-20 01:08:46 +1000

    Убрал из структуры все, что мне не понадобится

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
 
 func GetSomething(w http.ResponseWriter, r *http.Request) {
-	
+
     var req JsonMainRequest
     var res JsonMainResponse
 
@@ -108,104 +108,22 @@ func ExampleNewClient(request *JsonMainRequest) int {
 }
 
 
-
 type IfaData struct {
 	Count int `json:"count"`
 	Time  int `json:"time"`
 }
 
 
-
 type JsonMainRequest struct {
 	App struct {
-		Bundle    string   `json:"bundle"`
-		Cat       []string `json:"cat"`
-		ID        string   `json:"id"`
-		Name      string   `json:"name"`
-		Publisher struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"publisher"`
-		Ver string `json:"ver"`
+		Bundle string `json:"bundle"`
 	} `json:"app"`
-	At     int      `json:"at"`
-	Bcat   []string `json:"bcat"`
-	Cur    []string `json:"cur"`
 	Device struct {
-		Carrier        string `json:"carrier"`
-		ConnectionType int    `json:"connectiontype"`
-		Geo            struct {
-			Accuracy int    `json:"accuracy"`
-			City     string `json:"city"`
+		Geo struct {
 			Country  string `json:"country"`
-			Ext      struct {
-				OldGeo struct {
-					City    string `json:"city"`
-					Country string `json:"country"`
-					Region  string `json:"region"`
-					Zip     string `json:"zip"`
-				} `json:"old_geo"`
-			} `json:"ext"`
-			IpService int     `json:"ipservice"`
-			Lat       float64 `json:"lat"`
-			Lon       float64 `json:"lon"`
-			Region    string  `json:"region"`
-			Type      int     `json:"type"`
-			Utcoffset int     `json:"utcoffset"`
-			Zip       string  `json:"zip"`
 		} `json:"geo"`
-		H        int    `json:"h"`
-		Ifa      string `json:"ifa"`
-		IP       string `json:"ip"`
-		Js       int    `json:"js"`
-		Language string `json:"language"`
-		Make     string `json:"make"`
-		Model    string `json:"model"`
-		Os       string `json:"os"`
-		Osv      string `json:"osv"`
-		Pxratio  int    `json:"pxratio"`
-		Ua       string `json:"ua"`
-		W        int    `json:"w"`
+		Os string `json:"os"`
 	} `json:"device"`
-	Ext struct {
-		Envisionx struct {
-			Ssp int `json:"ssp"`
-		} `json:"envisionx"`
-	} `json:"ext"`
-	ID  string `json:"id"`
-	Imp []struct {
-		Banner struct {
-			API   []int `json:"api"`
-			Battr []int `json:"battr"`
-			Btype []int `json:"btype"`
-			H     int   `json:"h"`
-			Pos   int   `json:"pos"`
-			W     int   `json:"w"`
-		} `json:"banner"`
-		BidFloor          float64 `json:"bidfloor"`
-		DisplayManager    string  `json:"displaymanager"`
-		DisplayManagerVer string  `json:"displaymanagerver"`
-		Ext               struct {
-			Brsrclk int `json:"brsrclk"`
-			Dlp     int `json:"dlp"`
-		} `json:"ext"`
-		ID    string `json:"id"`
-		Instl int    `json:"instl"`
-		Tagid string `json:"tagid"`
-		Video struct {
-			API           []int    `json:"api"`
-			Battr         []int    `json:"battr"`
-			CompanionType []int    `json:"companiontype"`
-			H             int      `json:"h"`
-			Linearity     int      `json:"linearity"`
-			MaxDuration   int      `json:"maxduration"`
-			Mimes         []string `json:"mimes"`
-			MinDuration   int      `json:"minduration"`
-			Protocols     []int    `json:"protocols"`
-			Sequence      int      `json:"sequence"`
-			W             int      `json:"w"`
-		} `json:"video"`
-	} `json:"imp"`
 }
 
 
