5e2ea130707e7ed905b15cf94cc405b4410e7383
Eugene Maruschin <maruschin@gmail.com>
2017-11-21 03:56:41 +1000

    Добавил пару записей в тест, для разнообразия

--- test/test.py
+++ test/test.py
@@ -3,5 +3,7 @@ import os
 
 #os.system('redis-server &')
 #os.system('go run main.go &')
-os.system('curl -sS  -H "Content-Type: application/json" --data @data.json http://localhost:8080/')
+os.system('curl -sS  -H "Content-Type: application/json" --data @data1.json http://localhost:8080/')
+os.system('curl -sS  -H "Content-Type: application/json" --data @data2.json http://localhost:8080/')
+os.system('curl -sS  -H "Content-Type: application/json" --data @data3.json http://localhost:8080/')
 #os.system('redis-cli shutdown')
